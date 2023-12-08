95c88c69bf62fe7be14b1a8a8f60e90bbd3ed40a
Geliang Tang <geliang.tang@linux.dev>
2023-12-08 18:14:15 +0800

    mptcp: drop addr_match and id_match
    This patch uses the newly defined helper mptcp_userspace_pm_get_entry()
    in mptcp_userspace_pm_append_new_local_addr(), and drop local variables
    addr_match and id_match to simplify the code.
    
    Signed-off-by: Geliang Tang <geliang.tang@linux.dev>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -30,51 +30,40 @@ static int mptcp_userspace_pm_append_new_local_addr(struct mptcp_sock *msk,
 						    bool needs_id)
 {
 	DECLARE_BITMAP(id_bitmap, MPTCP_PM_MAX_ADDR_ID + 1);
-	struct mptcp_pm_addr_entry *match = NULL;
 	struct sock *sk = (struct sock *)msk;
 	struct mptcp_pm_addr_entry *e;
-	bool addr_match = false;
-	bool id_match = false;
 	int ret = -EINVAL;
 
 	bitmap_zero(id_bitmap, MPTCP_PM_MAX_ADDR_ID + 1);
 
 	spin_lock_bh(&msk->pm.lock);
 	list_for_each_entry(e, &msk->pm.userspace_pm_local_addr_list, list) {
-		addr_match = mptcp_addresses_equal(&e->addr, &entry->addr, true);
-		if (addr_match && entry->addr.id == 0 && needs_id)
-			entry->addr.id = e->addr.id;
-		id_match = (e->addr.id == entry->addr.id);
-		if (addr_match && id_match) {
-			match = e;
-			break;
-		} else if (addr_match || id_match) {
-			break;
+		if (mptcp_addresses_equal_check_id(&e->addr, &entry->addr, true, entry->addr.id)) {
+			if (entry->addr.id == 0 && needs_id)
+				entry->addr.id = e->addr.id;
+			ret = entry->addr.id;
+			goto append_err;
 		}
 		__set_bit(e->addr.id, id_bitmap);
 	}
 
-	if (!match && !addr_match && !id_match) {
-		/* Memory for the entry is allocated from the
-		 * sock option buffer.
-		 */
-		e = sock_kmalloc(sk, sizeof(*e), GFP_ATOMIC);
-		if (!e) {
-			ret = -ENOMEM;
-			goto append_err;
-		}
-
-		*e = *entry;
-		if (!e->addr.id && needs_id)
-			e->addr.id = find_next_zero_bit(id_bitmap,
-							MPTCP_PM_MAX_ADDR_ID + 1,
-							1);
-		list_add_tail_rcu(&e->list, &msk->pm.userspace_pm_local_addr_list);
-		msk->pm.local_addr_used++;
-		ret = e->addr.id;
-	} else if (match) {
-		ret = entry->addr.id;
+	/* Memory for the entry is allocated from the
+	 * sock option buffer.
+	 */
+	e = sock_kmalloc(sk, sizeof(*e), GFP_ATOMIC);
+	if (!e) {
+		ret = -ENOMEM;
+		goto append_err;
 	}
+
+	*e = *entry;
+	if (!e->addr.id && needs_id)
+		e->addr.id = find_next_zero_bit(id_bitmap,
+						MPTCP_PM_MAX_ADDR_ID + 1,
+						1);
+	list_add_tail_rcu(&e->list, &msk->pm.userspace_pm_local_addr_list);
+	msk->pm.local_addr_used++;
+	ret = e->addr.id;
 
 append_err:
 	spin_unlock_bh(&msk->pm.lock);
--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -676,6 +676,13 @@ bool mptcp_addresses_equal(const struct mptcp_addr_info *a,
 			   const struct mptcp_addr_info *b, bool use_port);
 void mptcp_local_address(const struct sock_common *skc, struct mptcp_addr_info *addr);
 
+static inline bool mptcp_addresses_equal_check_id(const struct mptcp_addr_info *a,
+						  const struct mptcp_addr_info *b,
+						  bool use_port, bool check_id)
+{
+	return mptcp_addresses_equal(a, b, use_port) ? a->id == b->id : false;
+}
+
 /* called with sk socket lock held */
 int __mptcp_subflow_connect(struct sock *sk, const struct mptcp_addr_info *loc,
 			    const struct mptcp_addr_info *remote);
