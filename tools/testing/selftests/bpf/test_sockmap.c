ff2a1657377852e042d7cbaaae56836dedc71842
Geliang Tang <tanggeliang@kylinos.cn>
2024-04-03 16:08:21 +0800

    selftests/bpf: Add F_SETFL for fcntl in test_sockmap
    Incorrect arguments are passed to fcntl() in test_sockmap.c when invoking
    it to set file status flags. If O_NONBLOCK is used as 2nd argument and
    passed into fcntl, -EINVAL will be returned (See do_fcntl() in fs/fcntl.c).
    The correct approach is to use F_SETFL as 2nd argument, and O_NONBLOCK as
    3rd one.
    
    Fixes: 16962b2404ac ("bpf: sockmap, add selftests")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Acked-by: Yonghong Song <yonghong.song@linux.dev>

--- tools/testing/selftests/bpf/test_sockmap.c
+++ tools/testing/selftests/bpf/test_sockmap.c
@@ -685,7 +685,9 @@ static int msg_loop(int fd, int iov_count, int iov_length, int cnt,
 		int push = 0;
 		fd_set w;
 
-		fcntl(fd, fd_flags);
+		if (fcntl(fd, F_SETFL, fd_flags))
+			goto out_errno;
+
 		/* Account for pop bytes noting each iteration of apply will
 		 * call msg_pop_data helper so we need to account for this
 		 * by calculating the number of apply iterations. Note user
