eed371fb11b9c1c09f17a9efed7ce005930c7684
Geliang Tang <tanggeliang@kylinos.cn>
2024-04-08 15:41:43 +0800

    selftests/bpf: Support nonblock for send_recv_data
    Some tests, such as the MPTCP bpf tests, require send_recv_data helper
    to run in nonblock mode.
    
    This patch adds nonblock support for send_recv_data(). Check if it is
    currently in nonblock mode, and if so, ignore EWOULDBLOCK to continue
    sending and receiving.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/network_helpers.c
+++ tools/testing/selftests/bpf/network_helpers.c
@@ -580,6 +580,7 @@ struct send_recv_arg {
 static void *send_recv_server(void *arg)
 {
 	struct send_recv_arg *a = (struct send_recv_arg *)arg;
+	int flags = fcntl(a->fd, F_GETFL);
 	ssize_t nr_sent = 0, bytes = 0;
 	char batch[1500];
 	int err = 0, fd;
@@ -603,6 +604,8 @@ static void *send_recv_server(void *arg)
 		if (nr_sent == -1 && errno == EINTR)
 			continue;
 		if (nr_sent == -1) {
+			if (flags & O_NONBLOCK && errno == EWOULDBLOCK)
+				continue;
 			err = -errno;
 			break;
 		}
@@ -627,6 +630,7 @@ done:
 
 int send_recv_data(int lfd, int fd, uint32_t total_bytes)
 {
+	int flags = fcntl(lfd, F_GETFL);
 	ssize_t nr_recv = 0, bytes = 0;
 	struct send_recv_arg arg = {
 		.fd	= lfd,
@@ -651,6 +655,8 @@ int send_recv_data(int lfd, int fd, uint32_t total_bytes)
 		if (nr_recv == -1 && errno == EINTR)
 			continue;
 		if (nr_recv == -1) {
+			if (flags & O_NONBLOCK && errno == EWOULDBLOCK)
+				continue;
 			err = -errno;
 			break;
 		}
