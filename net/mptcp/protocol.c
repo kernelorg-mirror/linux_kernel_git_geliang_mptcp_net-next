8727362708323ddee3c547b63d2dff4681848210
Geliang Tang <tanggeliang@kylinos.cn>
2024-04-19 15:46:57 +0800

    Revert "mptcp: react scheduler when subflow events pop up"
    This reverts commit 66f4e87a4e68668fb69f206001997ae5477675d1.

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -539,26 +539,6 @@ static long mptcp_timeout_from_subflow(const struct mptcp_subflow_context *subfl
 	       icsk_timeout(inet_csk(ssk)) - jiffies : 0;
 }
 
-static void __mptcp_subflow_timeout(struct sock *sk, struct sock *ssk, long tout)
-{
-	unsigned int boundary = READ_ONCE(sock_net(sk)->ipv4.sysctl_tcp_retries1) + 1;
-
-	if (retransmits_timed_out(ssk, boundary, tout))
-		__mptcp_check_push(sk, ssk);
-}
-
-static void mptcp_subflow_timeout(struct sock *sk)
-{
-	struct mptcp_subflow_context *subflow;
-
-	mptcp_for_each_subflow(mptcp_sk(sk), subflow) {
-		struct sock *ssk = mptcp_subflow_tcp_sock(subflow);
-		long tout = mptcp_timeout_from_subflow(subflow);
-
-		__mptcp_subflow_timeout(sk, ssk, tout);
-	}
-}
-
 void mptcp_set_timeout(struct sock *sk)
 {
 	struct mptcp_subflow_context *subflow;
@@ -2798,7 +2778,6 @@ static void mptcp_worker(struct work_struct *work)
 	mptcp_check_fastclose(msk);
 
 	mptcp_pm_worker(msk);
-	mptcp_subflow_timeout(sk);
 
 	mptcp_check_send_data_fin(sk);
 	mptcp_check_data_fin_ack(sk);
