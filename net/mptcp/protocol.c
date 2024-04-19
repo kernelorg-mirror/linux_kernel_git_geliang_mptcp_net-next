9e14e204448d83e1c640d9bb3fcd2f91444820c2
Geliang Tang <tanggeliang@kylinos.cn>
2024-04-19 15:46:34 +0800

    Revert "mptcp: drop 'first' in __mptcp_subflow_push_pending"
    This reverts commit 9d5b4f8a5fc7951d7d347b72b391eaa7f97e3503.

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -1700,7 +1700,7 @@ void __mptcp_push_pending(struct sock *sk, unsigned int flags)
 		mptcp_check_send_data_fin(sk);
 }
 
-static void __mptcp_subflow_push_pending(struct sock *sk, struct sock *ssk)
+static void __mptcp_subflow_push_pending(struct sock *sk, struct sock *ssk, bool first)
 {
 	struct mptcp_sock *msk = mptcp_sk(sk);
 	struct mptcp_sendmsg_info info = {
@@ -1714,6 +1714,19 @@ static void __mptcp_subflow_push_pending(struct sock *sk, struct sock *ssk)
 	while (mptcp_send_head(sk) && keep_pushing) {
 		struct mptcp_subflow_context *subflow = mptcp_subflow_ctx(ssk);
 		int ret = 0;
+
+		/* check for a different subflow usage only after
+		 * spooling the first chunk of data
+		 */
+		if (first) {
+			mptcp_subflow_set_scheduled(subflow, false);
+			ret = __subflow_push_pending(sk, ssk, &info);
+			first = false;
+			if (ret <= 0)
+				break;
+			copied += ret;
+			continue;
+		}
 
 		if (mptcp_sched_get_send(msk))
 			goto out;
@@ -3454,7 +3467,7 @@ void __mptcp_check_push(struct sock *sk, struct sock *ssk)
 		return;
 
 	if (!sock_owned_by_user(sk))
-		__mptcp_subflow_push_pending(sk, ssk);
+		__mptcp_subflow_push_pending(sk, ssk, false);
 	else
 		__set_bit(MPTCP_PUSH_PENDING, &mptcp_sk(sk)->cb_flags);
 }
@@ -3552,7 +3565,7 @@ void mptcp_subflow_process_delegated(struct sock *ssk, long status)
 	if (status & BIT(MPTCP_DELEGATE_SEND)) {
 		mptcp_data_lock(sk);
 		if (!sock_owned_by_user(sk))
-			__mptcp_subflow_push_pending(sk, ssk);
+			__mptcp_subflow_push_pending(sk, ssk, true);
 		else
 			__set_bit(MPTCP_PUSH_PENDING, &mptcp_sk(sk)->cb_flags);
 		mptcp_data_unlock(sk);
