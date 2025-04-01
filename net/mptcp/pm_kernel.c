c53870effefe8db2f6ef61c742b033f9a9a2729f
Geliang Tang <tanggeliang@kylinos.cn>
2025-04-01 09:15:18 +0800

    mptcp: pm: add accept_new_address() interface
    Similar to the accept_new_subflow() interface, this patch adds a mandatory
    accept_new_address() interface for struct mptcp_pm_ops, which is used to
    determine whether an ADD_ADDR address is accepted by PM.
    
    This interface is called in mptcp_pm_add_addr_received(). If the address is
    not accepted, increase MPTCP_MIB_ADDADDRDROP counter and return.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/mptcp/pm_kernel.c
+++ net/mptcp/pm_kernel.c
@@ -1582,6 +1582,13 @@ static bool mptcp_pm_kernel_accept_new_subflow(struct mptcp_sock *msk,
 	return ret;
 }
 
+static bool mptcp_pm_kernel_accept_new_address(struct mptcp_sock *msk,
+					       const struct mptcp_addr_info *addr)
+{
+	return !(mptcp_pm_accept_address(msk, addr) &&
+		 msk->pm.status & BIT(MPTCP_PM_ADD_ADDR_RECEIVED));
+}
+
 static void mptcp_pm_kernel_init(struct mptcp_sock *msk)
 {
 	bool subflows_allowed = !!mptcp_pm_get_limit_extra_subflows(msk);
@@ -1608,6 +1615,7 @@ struct mptcp_pm_ops mptcp_pm_kernel = {
 	.accept_new_subflow	= mptcp_pm_kernel_accept_new_subflow,
 	.established		= mptcp_pm_kernel_established,
 	.subflow_established	= mptcp_pm_kernel_subflow_established,
+	.accept_new_address	= mptcp_pm_kernel_accept_new_address,
 	.init			= mptcp_pm_kernel_init,
 	.name			= "kernel",
 	.owner			= THIS_MODULE,
