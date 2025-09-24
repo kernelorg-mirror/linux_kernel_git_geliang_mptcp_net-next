15720decb038029ccb3114c656c6863217148d30
Geliang Tang <tanggeliang@kylinos.cn>
2025-09-24 13:40:37 +0800

    selftests/bpf: Add mptcp_subflow bpf_iter subtest
    This patch adds a "cgroup/getsockopt" program "iters_subflow" to test the
    newly added mptcp_subflow bpf_iter.
    
    Export mptcp_subflow helpers bpf_iter_mptcp_subflow_new/_next/_destroy
    and other helpers into bpf_experimental.h.
    
    Use bpf_for_each() to walk the subflow list of an msk. From there,
    future MPTCP-specific kfunc can be called in the loop. Because they are
    not there yet, this test doesn't do anything very "useful" for the
    moment, but it focuses on validating the 'bpf_iter' part and the basic
    MPTCP kfunc. That's why it simply adds all subflow ids to local variable
    local_ids to make sure all subflows have been seen, then invoke
    mptcp_subflow_tcp_sock() in the loop to pick the subflow context.
    
    Out of the loop, use bpf_mptcp_subflow_ctx() to get the subflow context
    of the picked subflow context and do some verifications. Finally, assign
    local_ids to global variable ids so that the application can obtain this
    value.
    
    A related subtest called test_iters_subflow is added to load and verify
    the newly added mptcp_subflow type bpf_iter example in test_mptcp. The
    endpoint_init() helper is used to add 3 new subflow endpoints. Then one
    byte of message is sent to trigger the creation of new subflows.
    getsockopt() is invoked once the subflows have been created to trigger
    the "cgroup/getsockopt" test program "iters_subflow". skel->bss->ids is
    then checked to make sure it equals 10, the sum of each subflow ID: we
    should have 4 subflows: 1 + 2 + 3 + 4 = 10. If that's the case, the
    bpf_iter loop did the job as expected.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -330,6 +330,9 @@ static int bpf_mptcp_common_kfunc_filter(const struct bpf_prog *prog, u32 kfunc_
 	if (!btf_id_set8_contains(&bpf_mptcp_common_kfunc_ids, kfunc_id))
 		return 0;
 
+	if (prog->type == BPF_PROG_TYPE_CGROUP_SOCKOPT)
+		return 0;
+
 	if (prog->type != BPF_PROG_TYPE_STRUCT_OPS)
 		return -EACCES;
 
@@ -354,6 +357,10 @@ static int __init bpf_mptcp_kfunc_init(void)
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_STRUCT_OPS,
 					       &bpf_mptcp_iter_kfunc_set);
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_STRUCT_OPS,
+					       &bpf_mptcp_common_kfunc_set);
+	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_CGROUP_SOCKOPT,
+					       &bpf_mptcp_iter_kfunc_set);
+	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_CGROUP_SOCKOPT,
 					       &bpf_mptcp_common_kfunc_set);
 #ifdef CONFIG_BPF_JIT
 	ret = ret ?: register_bpf_struct_ops(&bpf_mptcp_sched_ops, mptcp_sched_ops);
