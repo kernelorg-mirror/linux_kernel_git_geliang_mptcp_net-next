geliangtang/mptcp_net-next

mptcp: add mptcp_pm_get_local_addr_list
Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

net/mptcp/pm_kernel.c
@@ -78,6 +78,14 @@ unsigned int mptcp_pm_get_local_addr_max(const struct mptcp_sock *msk)
 }
 EXPORT_SYMBOL_GPL(mptcp_pm_get_local_addr_max);
 
+struct list_head *mptcp_pm_get_local_addr_list(const struct mptcp_sock *msk)
+{
+	struct pm_nl_pernet *pernet = pm_nl_get_pernet_from_msk(msk);
+
+	return &pernet->local_addr_list;
+}
+EXPORT_SYMBOL_GPL(mptcp_pm_get_local_addr_list);
+
 static bool lookup_subflow_by_daddr(const struct list_head *list,
 				    const struct mptcp_addr_info *daddr)
 {

kernelorg-mirror/linux_kernel_git_geliang_mptcp_net-next

net/mptcp/pm_kernel.c
@@ -86,6 +86,14 @@ u8 mptcp_pm_get_limit_extra_subflows(const struct mptcp_sock *msk)
 }
 EXPORT_SYMBOL_GPL(mptcp_pm_get_limit_extra_subflows);
 
+struct list_head *mptcp_pm_get_local_addr_list(const struct mptcp_sock *msk)
+{
+	struct pm_nl_pernet *pernet = pm_nl_get_pernet_from_msk(msk);
+
+	return &pernet->endp_list;
+}
+EXPORT_SYMBOL_GPL(mptcp_pm_get_local_addr_list);
+
 static bool lookup_subflow_by_daddr(const struct list_head *list,
 				    const struct mptcp_addr_info *daddr)
 {